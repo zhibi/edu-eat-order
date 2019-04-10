e81921b4e49266b1992f7d83aa7d6bb7e36648f2
执笔 <805073259@qq.com>
2019-04-10 18:29:19 +0800

    发布评论

--- src/main/java/edu/eat/order/controller/CommentController.java
+++ src/main/java/edu/eat/order/controller/CommentController.java
@@ -1,15 +1,16 @@
 package edu.eat.order.controller;
 
 import edu.eat.order.base.annocation.RequestLogin;
+import edu.eat.order.base.base.controller.BaseController;
 import edu.eat.order.base.mybatis.condition.MybatisCondition;
 import edu.eat.order.domain.Business;
 import edu.eat.order.domain.Comment;
 import edu.eat.order.mapper.BusinessMapper;
 import edu.eat.order.mapper.CommentMapper;
-import edu.eat.order.base.base.controller.BaseController;
 import edu.eat.order.model.CommentModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,6 @@ import java.util.Date;
 import java.util.List;
 
 /**
- *
  * @author 执笔
  * @date 2019/4/10 18:23
  */
@@ -32,19 +32,27 @@ public class CommentController extends BaseController {
     @Autowired
     private BusinessMapper businessMapper;
 
+    /**
+     * 发布评论
+     *
+     * @param comment
+     * @return
+     */
+    @Transactional
     @RequestMapping("send")
     public String send(Comment comment) {
         comment.setAddtime(new Date());
         comment.setUserId(sessionUser().getId());
         commentMapper.insertSelective(comment);
         Business business = businessMapper.selectByPrimaryKey(comment.getBusinessId());
-       // business.setComment(business.getComment() + 1);
+        business.setCommendNum(business.getCommendNum() + 1);
         businessMapper.updateByPrimaryKeySelective(business);
         return refresh();
     }
 
     /**
      * 商户的评论
+     *
      * @param businessId
      * @param model
      * @return
